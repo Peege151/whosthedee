efe77dd2fbdcd35ecbb2fdf3971db09c943af965
Peege151 <Pjsulli2k@gmail.com>
2014-03-07 14:35:34 -0500

    Can only edit own information now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     before_action :signed_in_user, only: [:edit, :update]
+    before_action :correct_user,   only: [:edit, :update]
+
 
   def new
   	    @user = User.new
@@ -8,7 +10,6 @@ class UsersController < ApplicationController
     @users = User.all
   end
   def show
-        @user = User.find(params[:id])
         @user = User.find(params[:id])
   end
   def edit
@@ -38,10 +39,12 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-
     # Before filters
-
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
    def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.hash(remember_token))
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
   end
     def current_user=(user)
@@ -15,6 +15,9 @@ module SessionsHelper
     @current_user ||= User.find_by(remember_token: remember_token)
   end
 
+  def current_user?(user)
+    user == current_user
+  end
     def signed_in?
     !current_user.nil?
   	end
