813798b3f0c43a606500d6b059f115f62e9a27ba
Peege151 <Pjsulli2k@gmail.com>
2014-03-07 14:21:18 -0500

    Can Edit Anyone but only after login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,20 @@
 class UsersController < ApplicationController
+    before_action :signed_in_user, only: [:edit, :update]
+
   def new
   	    @user = User.new
   end
-
-   def index
+  def index
     @users = User.all
   end
   def show
         @user = User.find(params[:id])
-    @user = User.find(params[:id])
-
+        @user = User.find(params[:id])
   end
-    def create
+  def edit
+    @user = User.find(params[:id])
+  end
+  def create
     @user = User.new(user_params)
     if @user.save
      flash[:success] = "Welcome to the Sample App!"
@@ -19,9 +22,26 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-  end
-  
+  end 
+    def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end 
+    private
+
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 end
