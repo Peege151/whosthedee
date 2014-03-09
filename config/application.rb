cf0b73fe05f6a292e5f2885f513cb756ea31274d
Peege151 <Pjsulli2k@gmail.com>
2014-03-09 11:44:46 -0400

    Remove Production TItle On Gem File to See if Heroku SErves Assets

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,9 @@ module Whosthedee2
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     I18n.enforce_available_locales = true
+    config.serve_static_assets = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
 
   end
 end
