087816c919488215bd8e4b9ecfe839ce9d042efe
Peege151 <Pjsulli2k@gmail.com>
2014-03-24 12:24:55 -0400

    Pre Seed, DB Safe at this commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+City.create(name: 'New York City')
+City.create(name: 'Boston')
